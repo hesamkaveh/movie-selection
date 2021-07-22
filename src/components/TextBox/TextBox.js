30da78cd0e2a23b4a7aafabefb9036d8617692be
Hesam Kaveh <hk.hesam.hk@gmail.com>
2021-07-22 18:59:24 +0430

    added tooltip

--- src/components/TextBox/TextBox.js
+++ src/components/TextBox/TextBox.js
@@ -2,20 +2,24 @@ import React, {memo} from "react";
 import AddIcon from "@material-ui/icons/Add";
 import {useStyles} from "./TextBox.style";
 import CloseIcon from "@material-ui/icons/Close";
+import {Tooltip} from "@material-ui/core";
 
 export const TextBox = memo(({handleClick, children, variant = 'add'}) => {
     const classes = useStyles({variant})
 
     return (
+        <Tooltip title={children} aria-label={children}>
         <span
             className={classes.eachSelected}
             onClick={handleClick}>
             {variant === 'add'
                 ? <AddIcon fontSize={'small'}/>
                 : <CloseIcon fontSize={'small'}/>}
+
             <span>
                 {children}
             </span>
         </span>
+        </Tooltip>
     )
 })
